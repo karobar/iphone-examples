b785b8637fba9295e7450cc71ccd98dfb51ded5d
RangDipkin <travisp471@gmail.com>
2015-06-10 03:12:10 -0400

    removed extraneous folders

--- a4-b481-template/build/a4-b481.build/Debug-iphonesimulator/a4-b481.build/Objects-normal/i386/MaterialManager.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/mitja/Desktop/481-2014-Spring/assignments-b481/a4-b481/a4-b481-template/a4-b481/Src/MaterialManager.cpp \
-  /Users/mitja/Desktop/481-2014-Spring/assignments-b481/a4-b481/a4-b481-template/a4-b481/Src/MaterialManager.h \
-  glm/../rapidxml/rapidxml.hpp
--- a4-b481-template/build/a4-b481.build/Debug-iphonesimulator/a4-b481.build/Objects-normal/i386/ResourcePath.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/mitja/Desktop/481-2014-Spring/assignments-b481/a4-b481/a4-b481-template/a4-b481/Src/ResourcePath.mm \
-  /Users/mitja/Desktop/481-2014-Spring/assignments-b481/a4-b481/a4-b481-template/a4-b481/Src/ResourcePath.h
